3920083270a58f1f7e738391ccd93c0389394ffe
Gismo <gismo2501@kggaming>
2022-02-22 15:22:25 +0100

    Comments

--- condition_folder_renamer/renamer.py
+++ condition_folder_renamer/renamer.py
@@ -13,16 +13,21 @@ def main():
     print("### Bitte noch nicht in einer Produktivumgebung benutzen! ###")
 
     while True:
+        # Get file destination
         input_csv_file = str(input("Bitte gib den Pfad der CSV Datei an!"))
+        # check if input is empty
         if input_csv_file == "":
             print("Der Pfad darf nicht leer sein!")
             continue
+        # check if input contains .csv
         if not input_csv_file.__contains__(".csv"):
             print("Bitte gib eine Datei mit der Dateiendung .csv an!")
             continue
+        # User check if destination is correct
         print("Ist der Pfad korrekt? \n" + '"' + input_csv_file + '"')
         input_correct_csv_file = str(input("Y für Ja | N für Nein | Andere Tasten für Abbruch"))
 
+        # Switch Case for these options
         match input_correct_csv_file:
             case "Y":
                 break
@@ -37,13 +42,17 @@ def main():
                 break
 
     while True:
+        # input folder destination
         input_folder = str(input("Bitte gib den Pfad der Ordner an!"))
+        # check if folder is empty
         if input_folder == "":
             print("Der Pfad darf nicht leer sein!")
             continue
+        # User check if destination is correct
         print("Ist der Pfad korrekt? \n" + '"' + input_folder + '"')
         input_correct_folder = str(input("Y für Ja | N für Nein | Andere Tasten für Abbruch"))
 
+        # Switch Case of these options
         match input_correct_folder:
             case "Y":
                 break
@@ -56,6 +65,10 @@ def main():
             case _:
                 print("Das Programm wurde abgrebochen!")
                 break
+
+# just the main method. Nothing special otter.
+# Don't suspect me to do smth stupid :(
+# Dragons can code | Okay i need to delete these comments
 
 
 if __name__ == "__main__":
