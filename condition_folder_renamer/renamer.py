c329911f54183580ff89afe88a6c66b714f2789c
Gismo <gismo2501@kggaming>
2022-02-22 18:10:32 +0100

    Beta Version 1.0 -> CSV Check -> Check if folder is available -> rename folder with the given pattern (readme)

--- condition_folder_renamer/renamer.py
+++ condition_folder_renamer/renamer.py
@@ -5,8 +5,13 @@
 
 """
 
+# Imports
+import csv
+import os
+
 
 def main():
+    global nichtvorhanden
     print("### Willkommen beim Folder Renamer von Condition ###")
     # Remove print statements before release!!!!
     print("### Bitte beachten Sie, dass diese Version noch in der Alpha Phase ist! ###")
@@ -65,6 +70,119 @@ def main():
             case _:
                 print("Das Programm wurde abgrebochen!")
                 break
+    try:
+        nichtvorhanden = []
+        with open(input_csv_file) as csvdatei:
+            csv_reader_object = csv.reader(csvdatei, delimiter=";")
+            for row in csv_reader_object:
+                if row.__contains__("Name"):
+                    print("Header")
+                else:
+                    print(row[0])
+                    if not os.listdir(input_folder).__contains__(row[0]):
+                        print("Nicht vorhanden!")
+                        nichtvorhanden.append(row[0])
+                    else:
+                        print(row[0]+" ist vorhanden!")
+    except FileNotFoundError:
+        print("Die Datei ist unter: " + input_csv_file + " nicht vorhanden!"
+                                                         "\nBitte starte das Programm erneut und gib den richtigen "
+                                                         "Pfad an!")
+
+    print("Folgende Ordner sind nicht vorhanden: ", nichtvorhanden)
+
+    print("Möchten Sie mit der Umbennenung fortfahren?")
+    input_continue = str(input("Y für Ja | N für Nein | Andere Tasten für Abbruch"))
+
+    # Switch Case of these options
+    match input_continue:
+        case "Y":
+            rename_folder(input_folder, input_csv_file)
+        case "y":
+            rename_folder(input_folder, input_csv_file)
+        case "N":
+            print("Das Programm wurde abgrebochen!")
+        case "n":
+            print("Das Programm wurde abgrebochen!")
+        case _:
+            print("Das Programm wurde abgrebochen!")
+
+
+def rename_folder(folder, csv_file):
+    try:
+        with open(csv_file) as csvdatei:
+            csv_reader_object = csv.reader(csvdatei, delimiter=";")
+            for row in csv_reader_object:
+                if row.__contains__("Name") and row.__contains__("Owner"):
+                    print("Header")
+                else:
+                    if os.listdir(folder).__contains__(row[0]):
+                        if row[0].__contains__("KUN") or row[0].__contains__("WO"):
+                            match row[1]:
+                                case "CONDITION\\MO":
+                                    name = row[1]
+                                    os.rename(folder+"\\"+row[0], folder+"\\"+row[0]+"_"+name[10:])
+                                    print(row[0] + " wurde in ", row[0]+"_"+name[10:], " verändert!")
+                                case "CONDITION\\MM":
+                                    name = row[1]
+                                    os.rename(folder+"\\"+row[0], folder+"\\"+row[0]+"_"+name[10:])
+                                    print(row[0] + " wurde in ", row[0]+"_"+name[10:], " verändert!")
+                                case "CONDITION\\PD":
+                                    name = row[1]
+                                    os.rename(folder+"\\"+row[0], folder+"\\"+row[0]+"_"+name[10:])
+                                    print(row[0] + " wurde in ", row[0]+"_"+name[10:], " verändert!")
+                                case "CONDITION\\SL":
+                                    name = row[1]
+                                    os.rename(folder+"\\"+row[0], folder+"\\"+row[0]+"_"+name[10:])
+                                    print(row[0] + " wurde in ", row[0]+"_"+name[10:], " verändert!")
+                                case "CONDITION\\OR":
+                                    name = row[1]
+                                    os.rename(folder+"\\"+row[0], folder+"\\"+row[0]+"_"+name[10:])
+                                    print(row[0] + " wurde in ", row[0]+"_"+name[10:], " verändert!")
+                                case "CONDITION\\DW":
+                                    name = row[1]
+                                    os.rename(folder+"\\"+row[0], folder+"\\"+row[0]+"_"+name[10:])
+                                    print(row[0] + " wurde in ", row[0]+"_"+name[10:], " verändert!")
+                                case "CONDITION\\TS":
+                                    name = row[1]
+                                    os.rename(folder+"\\"+row[0], folder+"\\"+row[0]+"_"+name[10:])
+                                    print(row[0] + " wurde in ", row[0]+"_"+name[10:], " verändert!")
+                                # lower
+                                case "CONDITION\\mo":
+                                    name = row[1]
+                                    os.rename(folder+"\\"+row[0], folder+"\\"+row[0]+"_"+name[10:])
+                                    print(row[0] + " wurde in ", row[0]+"_"+name[10:], " verändert!")
+                                case "CONDITION\\mm":
+                                    name = row[1]
+                                    os.rename(folder+"\\"+row[0], folder+"\\"+row[0]+"_"+name[10:])
+                                    print(row[0] + " wurde in ", row[0]+"_"+name[10:], " verändert!")
+                                case "CONDITION\\pd":
+                                    name = row[1]
+                                    os.rename(folder+"\\"+row[0], folder+"\\"+row[0]+"_"+name[10:])
+                                    print(row[0] + " wurde in ", row[0]+"_"+name[10:], " verändert!")
+                                case "CONDITION\\sl":
+                                    name = row[1]
+                                    os.rename(folder+"\\"+row[0], folder+"\\"+row[0]+"_"+name[10:])
+                                    print(row[0] + " wurde in ", row[0]+"_"+name[10:], " verändert!")
+                                case "CONDITION\\or":
+                                    name = row[1]
+                                    os.rename(folder+"\\"+row[0], folder+"\\"+row[0]+"_"+name[10:])
+                                    print(row[0] + " wurde in ", row[0]+"_"+name[10:], " verändert!")
+                                case "CONDITION\\dw":
+                                    name = row[1]
+                                    os.rename(folder+"\\"+row[0], folder+"\\"+row[0]+"_"+name[10:])
+                                    print(row[0] + " wurde in ", row[0]+"_"+name[10:], " verändert!")
+                                case "CONDITION\\ts":
+                                    name = row[1]
+                                    os.rename(folder+"\\"+row[0], folder+"\\"+row[0]+"_"+name[10:])
+                                    print(row[0] + " wurde in ", row[0]+"_"+name[10:], " verändert!")
+                                case _:
+                                    print(row[0]," mit Owner: "+row[1]," wurde nicht verändert!")
+    except FileNotFoundError:
+        print("Die Datei ist unter: " + csv_file + " nicht vorhanden!"
+                                                         "\nBitte starte das Programm erneut und gib den richtigen "
+                                                         "Pfad an!")
+
 
 # just the main method. Nothing special otter.
 # Don't suspect me to do smth stupid :(
@@ -73,5 +191,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
