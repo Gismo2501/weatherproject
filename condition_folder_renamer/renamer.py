fbceff48fe9bccc0e0579c0def9a0a43219e9f50
Gismo <gismo2501@kggaming>
2022-02-24 20:10:58 +0100

    Commented code for the little otter

--- condition_folder_renamer/renamer.py
+++ condition_folder_renamer/renamer.py
@@ -13,8 +13,11 @@ from datetime import datetime
 
 
 def main():
+    # get current date
     today = datetime.now()
+    # format date to "MM-DD-YYYY-HH_MM
     day = today.strftime("%b-%d-%Y-%H_%M")
+    # global variable
     global nichtvorhanden
     print("### Willkommen beim Folder Renamer von Condition ###")
     # Remove print statements before release!!!!
@@ -84,15 +87,22 @@ def main():
                 break
 
     try:
+        # init empty list for not available folder
         nichtvorhanden = []
+        # open CSV File
         with open(input_csv_file) as csvdatei:
+            # Read CSV File
             csv_reader_object = csv.reader(csvdatei, delimiter=";")
+            # Go through CSV File row for row
             for row in csv_reader_object:
+                # detects the header
                 if row.__contains__("Name"):
                     print("Header")
                 else:
+                    # checks if folder is available
                     print(row[0]+" checking...")
                     logging.info(row[0]+" checking...")
+                    # if folder is not available.. put it in the list and forget about it
                     if not os.listdir(input_folder).__contains__(row[0]):
                         print("Nicht vorhanden!")
                         logging.info(row[0] + " not available!")
@@ -100,6 +110,7 @@ def main():
                     else:
                         print(row[0]+" ist vorhanden!")
                         logging.info(row[0] + " found")
+    # catch filenotfounderror
     except FileNotFoundError:
         print("Die Datei ist unter: " + input_csv_file + " nicht vorhanden"
                                                          "\noder der angegebene Ordner ist nicht vorhanden!"
@@ -107,45 +118,60 @@ def main():
                                                          "Pfad an!")
         logging.error("File or Directory not found. Cancel program")
         quit()
-
+    # print not available folder
     print("Folgende Ordner sind nicht vorhanden: ", nichtvorhanden)
     for not_available in nichtvorhanden:
         logging.info(not_available + " is not available")
 
+    # user check if renaming should start
     print("Möchten Sie mit der Umbennenung fortfahren?")
     input_continue = str(input("Y für Ja | N für Nein | Andere Tasten für Abbruch\n"))
 
     # Switch Case of these options
     match input_continue:
         case "Y":
+            # start renaming
             rename_folder(input_folder, input_csv_file)
         case "y":
+            # start renaming
             rename_folder(input_folder, input_csv_file)
         case "N":
+            # kill program
             print("Das Programm wurde abgrebochen!")
             logging.error("Program cancelled due user interaction")
         case "n":
+            # kill program
             print("Das Programm wurde abgrebochen!")
             logging.error("Program cancelled due user interaction")
         case _:
+            # kill program
             print("Das Programm wurde abgrebochen!")
             logging.error("Program cancelled due user interaction")
 
 
 def rename_folder(folder, csv_file):
+    # open csv file and read it row for row
     logging.info("Starting renaming folder...")
     try:
         with open(csv_file) as csvdatei:
             csv_reader_object = csv.reader(csvdatei, delimiter=";")
             for row in csv_reader_object:
+                # check header
                 if row.__contains__("Name") and row.__contains__("Owner"):
                     print("Header")
                 else:
+                    # get list of dirs
                     if os.listdir(folder).__contains__(row[0]):
+                        # check if folder contains "KUN" or "WO" in name
                         if row[0].__contains__("KUN") or row[0].__contains__("WO"):
+                            # check if folder name not contains one of the Owner
                             if not row[0].__contains__("MO") or row[0].__contains__("mo") or row[0].__contains__("MM") or row[0].__contains__("mm") or row[0].__contains__("PD") or row[0].__contains__("pd")or row[0].__contains__("SL")or row[0].__contains__("sl")or row[0].__contains__("OR")or row[0].__contains__("or")or row[0].__contains__("DW")or row[0].__contains__("dw")or row[0].__contains__("TS")or row[0].__contains__("ts"):
+                                # Switchcase for every owner (Upper and Lowercase) and rename them within the given
+                                # schema
                                 match row[1]:
                                     case "CONDITION\\MO":
+                                        # get the name as string (not as list item)
+                                        # then slice the CONDITION\ from the Owners name to get the right suffix
                                         name = row[1]
                                         os.rename(folder+"\\"+row[0], folder+"\\"+row[0]+"_"+name[10:])
                                         print(row[0] + " wurde in ", row[0]+"_"+name[10:], " verändert!")
@@ -217,11 +243,14 @@ def rename_folder(folder, csv_file):
                                         print(row[0] + " wurde in ", row[0]+"_"+name[10:], " verändert!")
                                         logging.info(row[0] + " changed to " + row[0]+"_"+name[10:])
                                     case _:
+                                        # if not one of the given Owner, the folder will not be changed
                                         print(row[0]," mit Owner: "+row[1]," wurde nicht verändert!")
                                         logging.warning(row[0] + " with owner: "+row[1] + " did not change!")
                             else:
+                                # folders will not be changed if not in given schema
                                 print(row[0], " wurde nicht verändert.")
                                 logging.warning(row[0] + " did not change.")
+    # catch filenotfounderror
     except FileNotFoundError:
         print("Die Datei ist unter: " + csv_file + " nicht vorhanden!"
                                                          "\nBitte starte das Programm erneut und gib den richtigen "
@@ -230,9 +259,6 @@ def rename_folder(folder, csv_file):
 
 
 # just the main method. Nothing special otter.
-# Don't suspect me to do smth stupid :(
-# Dragons can code | Okay i need to delete these comments
-
 
 if __name__ == "__main__":
     main()
